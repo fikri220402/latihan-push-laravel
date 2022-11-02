31ebbc8355186b79e5e139d491eaca7f4ba50c23
fikri220402 <sofwanfikriilyasa@gmail.com>
2022-11-02 14:19:53 +0700

    Second Branch

--- perkenalan-apps/resources/views/dashboard/about.blade.php
+++ perkenalan-apps/resources/views/dashboard/about.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand" href="/dashboard">Perkenalan</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,13 +18,13 @@
                     <a class="nav-link" aria-current="page" href="/index">Home</a>
                     <a class="nav-link active" href="/about">About</a>
                     <a class="nav-link" href="/contact">Contact</a>
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link disabled">Mending Turu</a>
                 </div>
             </div>
         </div>
     </nav>
-    <h1>TENTANG SAYA</h1>
-    <p>Saya adalah pemuda yang tersesat di informatika</p>
+    <h1 class="text-center">TENTANG SAYA</h1>
+    <p class="text-center">Saya adalah pemuda yang tersesat di Informatika</p>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
